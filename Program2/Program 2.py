d5821026863cec88f7afee32b746ac2df9b033b0
Tu Vu <tuvu2607@gmail.com>
2020-05-05 12:51:45 -0700

    Update SD for special case
    Also change the way the array is sent in to ignore the last column

--- Program2/Program 2.py
+++ Program2/Program 2.py
@@ -16,24 +16,30 @@ class Bayes(object):
     #Return a tuple of spam% and nonspam% for each class of training data
     def priorProb(self,array):
         spam = np.count_nonzero(array) #Count all the non zeroes 
-        spam_percent = (spam / 57) * 100
+        spam_percent = (spam / len(array)) * 100
         nonspam_percent = 100 - spam_percent
         return spam_percent, nonspam_percent
 
     #Return the mean and standard deviation of each class
     def mean_SD(self,array):
-        return np.mean(array), np.std(array)
+        std = np.std(array)
+        if(std == 0):
+            std == 0.0001 # hard code value
+            return np.mean(array), np.std(array)
+        else:
+            return np.mean(array), std
 
     #The last column of the spambase data denote whether the email is spam(1) or not spam(0)
+    #That last column could be the target that we use.
+    #Check the document on the database online
     def probModel(self):
         prior = []
         mean_SD = []
         for i in range(2300):
-            spam, nonspam = self.priorProb(self.trainData[i,:])
+            spam, nonspam = self.priorProb(self.trainData[i,:-2])
             prior.append((spam,nonspam)) 
-            mean_SD.append(self.mean_SD(self.trainData[i,:]))
+            mean_SD.append(self.mean_SD(self.trainData[i,:-2]))
         return np.asarray(prior), np.asarray(mean_SD)
     
     
 a = Bayes("spambase.data")
-
