9d4479c8d659f2f6024c0d0ce372b602e65edeef
Tu Vu <tuvu2607@gmail.com>
2020-05-04 19:49:29 -0700

    Added in a test prior probability

--- Program2/Program 2.py
+++ Program2/Program 2.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import GaussianNB
 
 class Bayes(object):
     def __init__(self, filename):
@@ -11,10 +12,20 @@ class Bayes(object):
         testData = data[2301:,:] #Selecting 2300 rows for the testData set, 2nd half of the data
         return trainData, testData
 
-    #Template for later
+    #Return a tuple of spam% and nonspam% for each class of training data
+    def priorProb(self,array):
+        spam = np.count_nonzero(array) #Count all the non zeroes 
+        spam_percent = (spam / 57) * 100
+        nonspam_percent = 100 - spam_percent
+        return spam_percent, nonspam_percent
+
+    #The last column of the spambase data denote whether the email is spam(1) or not spam(0)
     def probModel(self):
-        return 0    
+        prior = []
+        for i in range(2300):
+            spam, nonspam = self.priorProb(self.trainData[i,:])
+            prior.append((spam,nonspam)) 
+        return np.array(prior)
 
 a = Bayes("spambase.data")
-print(a.trainData.shape)
-print(a.testData.shape)
+b = a.probModel()
