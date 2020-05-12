caebe08af2dd512d30d88002e7d7db82b61dcd63
Tu Vu <tuvu2607@gmail.com>
2020-05-12 15:04:21 -0700

    Identified the problem
    Since a value in mean can be zero, if the value of the testData is zero, the exponent is practically 0 hence the divide by zero error

--- Program2/Program 2.py
+++ Program2/Program 2.py
@@ -1,6 +1,8 @@
 import numpy as np
+from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 class Bayes(object):
     def __init__(self, filename):
@@ -33,9 +35,13 @@ class Bayes(object):
         nonspam = np.asarray(nonspam)
         #Find the mean and std of each column within the spam and non spam array
         #For all 57 features
-        for i in range(features):
-            m[0,i] = np.mean(nonspam.T[i]) #Row 0 is the non spam
-            m[1,i] = np.mean(spam.T[i]) #Row 1 is the spam
+        for i in range(0,features):
+            if np.count_nonzero(nonspam.T[i]) == 0:
+                print("We got a zero column in non spam")
+            if np.count_nonzero(spam.T[i]) == 0:
+                print("We got a zero column in spam")
+            m[0,i] = np.nanmean(nonspam.T[i]) #Row 0 is the non spam
+            m[1,i] = np.nanmean(spam.T[i]) #Row 1 is the spam
             std[0,i] = np.std(nonspam.T[i]) #Row 0 is non spam
             std[1,i] = np.std(spam.T[i]) #Row 1 is spam
 
@@ -46,8 +52,10 @@ class Bayes(object):
             if std[1,j] == 0:
                 std[1,j] = 0.0001
 
-        print(min(std[0]))
-        print(min(std[1]))
+        print("Minimum value in mean rows:")
+        print(min(m[0]))
+        print(min(m[1])) 
+
         return m,std
 
     def probModel(self):
@@ -55,6 +63,7 @@ class Bayes(object):
         trainNonspam_percent = 1 - trainSpam_percent
 
         train_mean_tuple, train_std_tuple = self.mean_std()
+
         return trainSpam_percent, trainNonspam_percent, train_mean_tuple, train_std_tuple
 
     #Reminder that row 0 is non spam, and row 1 is spam
@@ -66,18 +75,20 @@ class Bayes(object):
         
         for i in range(2):
             p = 0
-            for j in range(len(x)):
-                a = (x[j] - mean[i][j])**2
-                b = 2 * ((std[i][j])**2)
-                exponent = np.exp(-(a/b))
-                N = 1 / (np.sqrt(2*np.pi) * std[i][j])
-                p += np.log(N * exponent)
+
             if i == 0:
                 p += np.log(nspam)
             elif i == 1:
                 p += np.log(spam)
+
+            for j in range(len(x)):
+                a = (x[j] - mean[i][j])**2 # <-- This is a problem. Apparently x[j] - mean[i][j] is 0
+                b = 2 * ((std[i][j])**2)
+                exponent = np.exp(-(a/b))
+                N = 1 / (np.sqrt(2*np.pi) * std[i][j])
+                p += np.log(N * exponent)
+
             pProb[i] = p
-        
         return pProb
 
     #Predict the class based on Naive Bayes Algorithm
@@ -101,7 +112,20 @@ class Bayes(object):
         accuracy = correct / len(self.testTarget) * 100
         return accuracy
 
+    #For comparison
+    def GB(self):
+        gnb = GaussianNB()
+        gnb.fit(self.trainData, self.trainTarget)
+        y_pred = gnb.predict(self.testData)
+        return metrics.accuracy_score(self.testTarget, y_pred)
+
+
 a = Bayes("spambase.data")
 b = a.predict()
 c = a.accuracy(b)
+d = a.GB()
+
+print(confusion_matrix(a.testTarget, b))
+
 print(c)
+print(d)
