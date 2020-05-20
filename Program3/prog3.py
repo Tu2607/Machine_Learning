eac0ea3957b23336d87ba06ec36a556195e3f6c8
Tu Vu <tuvu2607@gmail.com>
2020-05-19 23:44:39 -0700

    Rewrote the updateK function

--- Program3/prog3.py
+++ Program3/prog3.py
@@ -12,13 +12,15 @@ class kc_means(object):
         self.data = self.loadfile(filename)
         self.clusterCount = clusterCount
         self.iteration =  iteration
+
         self.K = self.selectK() 
+        self.store = []
 
     def loadfile(self,filename):
         data = np.loadtxt(filename)
         return data
 
-    #Note for this, not sure if np.asarray will be needed
+    #Initial means
     def selectK(self):
         k = []
         index = np.random.choice(self.data.shape[0], self.clusterCount, replace = False)
@@ -48,28 +50,28 @@ class kc_means(object):
 
     #Assigning each observation to a class
     def assignmentK(self):
-        K = [[] for _ in range(self.clusterCount)]
+        group = [[] for _ in range(self.clusterCount)]
 
         for i in range(self.data.shape[0]):
            distances = self.EuclideDistance(self.data[i])  #Calculate the mean distance for each observation against the Ks
-           K[self.classification(distances)].append(self.data[i])
+           group[self.classification(distances)].append(self.data[i])
         
-        return K
+        return group
 
     #Updating the K centroid   
     #cluster is the return value from function assignment 
     def updateK(self, cluster):
         assert (len(self.K) == len(cluster)), "Check in update()" #Check if the length of K is equal to the cluster we sent in
+        #Updating each centroid value
         for i in range(len(self.K)):
             if cluster[i]:  #To make sure that there is something in the cluster
                 clus = np.asarray(cluster[i]) ## Turn the list into a 2d array
-                print(clus.shape)
-                meanx = np.mean(clus.T[0]) ##I know this is hard coding
-                meany = np.mean(clus.T[1])
-                self.K[i] = [meanx,meany]
+                new = np.sum(clus, axis = 0) / clus.shape[0] # <-- CHECK MAH MATH HEREEEEE!!!!!
+                self.K[i] = new
+
 
     #Add plotting here 
-    #Might change
+    #Still need to add in the sum square error calculation
     def kmeans(self):
         pl.scatter(self.data.T[0], self.data.T[1])
         pl.scatter(self.K.T[0], self.K.T[1], c = 'r')
@@ -80,15 +82,20 @@ class kc_means(object):
             pl.scatter(self.data.T[0], self.data.T[1])
             pl.scatter(self.K.T[0], self.K.T[1], c = 'r')
             pl.show()
+            #Calculate Sum Square error here?
 
 
 #The main function that read in the argument of how many cluster point
 def main():
-    clusterCount = int(sys.argv[1])
-    iteration = int(sys.argv[2])
+    clusterCount = int(sys.argv[2])
+    iteration = int(sys.argv[3])
+    choice = str(sys.argv[1])
     a = kc_means("cluster_dataset.txt", clusterCount, iteration)
-    print(a.K)
-    a.kmeans()
+
+    if choice == 'k':
+        a.kmeans()
+    elif choice == 'c':
+        print("Wait here")
     
 
 if __name__ == '__main__':
